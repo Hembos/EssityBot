d04b4434b401fce8f0627294a27d90f32c08743c
Artur <faizrahmanov.artur@mail.ru>
2021-10-22 12:17:05 +0300

    initialisation

---
+++ config.py
@@ -0,0 +1,6 @@
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = environ.get("TOKEN")
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import telebot
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-if __name__ == "__main__":
-    print(123)
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(msg: types.Message):
+    await msg.answer(text=f"Привет, {msg.from_user.username}")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
